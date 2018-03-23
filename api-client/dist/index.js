4e7f7e4e88b814fc73869260dc2955b7a18b481c
Asier <asierp.dev@gmail.com>
2018-03-23 15:51:13 +0100

    prepare for first deployment

--- api-client/dist/index.js
+++ api-client/dist/index.js
@@ -5,13 +5,17 @@ var axios = require('axios');
 var bookBuddiesApi = {
     _url: function _url() {
         //  IT WILL BE THIS
-        // return `${this.protocol}://${this.host}:${this.port}/api`
-        return 'http://localhost:5555/api';
+        return 'https://aqueous-tundra-90892.herokuapp.com/api';
+        //return 'http://localhost:5555/api'
     },
 
 
     generalSearch: function generalSearch(query) {
         return axios.get(this._url() + '/results/' + query);
+    },
+
+    retrieveRandom: function retrieveRandom() {
+        return axios.get(this._url() + '/browse');
     },
 
     categorySearch: function categorySearch(query) {
